41370ace8752f1b75061f7e962d285a78e5c81a0
luhuadong <luhuadong@163.com>
2023-09-21 20:36:25 +0800

    add 矩阵替代 for 循环示例

---
+++ Matrix/no-loop-example/array_example.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#define DATA_WIDTH  8000
+#define DATA_HEIGHT 200
+
+// #define ENABLE_PRINT
+
+int main()
+{
+    // 定义一个包含3行4列的二维数组
+    int data[DATA_HEIGHT][DATA_WIDTH];
+
+    std::cout << "Origianl: " << std::endl;
+
+    auto start_time1 = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < DATA_HEIGHT; ++i) {
+        for (int j = 0; j < DATA_WIDTH; ++j) {
+            data[i][j] = i * DATA_WIDTH + j;
+        }
+    }
+
+    auto end_time1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_time1 = end_time1 - start_time1;
+    std::cout << "Elapsed Time: " << elapsed_time1.count() << "seconds" << std::endl;
+
+#ifdef ENABLE_PRINT
+    for (int i = 0; i < DATA_HEIGHT; ++i) {
+        for (int j = 0; j < DATA_WIDTH; ++j) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+#endif
+
+    std::cout << "Modified: " << std::endl;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    // 访问二维数组的元素
+    for (int i = 0; i < DATA_HEIGHT; ++i)
+    {
+        for (int j = 0; j < DATA_WIDTH; ++j)
+        {
+            data[i][j] = data[i][j] * 2;
+        }
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_time = end_time - start_time;
+
+#ifdef ENABLE_PRINT
+    for (int i = 0; i < DATA_HEIGHT; ++i) {
+        for (int j = 0; j < DATA_WIDTH; ++j) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+#endif
+
+    std::cout << "Elapsed Time: " << elapsed_time.count() << "seconds" << std::endl;
+
+    return 0;
+}
