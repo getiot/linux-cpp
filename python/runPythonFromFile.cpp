a0298df2ec32e2171a484503f266fb139d032b21
luhuadong <luhuadong@163.com>
2023-10-26 22:11:28 +0800

    [py] update runPythonFromFile.cpp example

--- python/runPythonFromFile.cpp
+++ python/runPythonFromFile.cpp
@@ -15,13 +15,14 @@ int main()
 
     py::exec("print('Hello World!')", main_namespace);
 
-    // /home/rudy/workspace/FusionPosition/Asensing_LiDAR_Driver/src/ag_driver/driver/decoder/A2/example.py
+#ifdef USING_IMPORT_MODULE
+    py::object module = py::import("example");
+    py::object add_func = module.attr("add");
+#else
     py::exec_file("example.py", main_namespace, main_namespace);
     py::object add_func = main_namespace["add"];
-    // py::object result = func(1, 2);
+#endif
 
-    // py::object module = py::import("example");
-    // py::object add_func = module.attr("add");
     int result = py::extract<int>(add_func(2, 3));
     std::cout << "The result is: " << result << std::endl;
 
